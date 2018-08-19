966fff18b564d208c9a54c473a25291359397953
Rapidfir3Pho3nix <official.rapidfir3pho3nix@gmail.com>
2018-08-19 10:24:47 -0500

    completely removed cleverbot and refactoring

--- bot.js
+++ bot.js
@@ -8,159 +8,102 @@ const client = new Discord.Client();
 //load settings file for SpritasBot
 const settings = require("./settings.json");
 const http = require("http");
-var fs = require('fs');
 
 client.on("ready", () => {
-    console.log("Bot is ready!");
+    let readyMsg = "Bot is ready!";
+    console.log(readyMsg);
     client.user.setPresence({ game: { name: "on The Spritas Discord Server" }});
     const spritas = client.guilds.get(settings.spritas_server);
-    spritas.owner.createDM().then((channel) => {
-        channel.send("Bot is ready!");
-    });
+    spritas.owner.createDM().then((channel) => { channel.send(readyMsg); });
 });
 
 client.on("message", async (message) => {
     //ignore messages from bots
     if(message.author.bot) return;
 
-    //talking to spritasbot
-    // if(message.mentions.users.has(client.user.id)) {
-    //     fs.readFile("cleverbot.json", (err, data) => {
-    //         if(err) console.log(err);
-    //         var clevState = JSON.parse(data);
-    //         let input = message.content.replace(/<(.*?)>/g, "").replace(/\s\s+/g, " ").trim().replace(/\\/, "");
-    //         console.log(message.author.username + " said \"" + input + "\"");
-    //         let queryUrl = "http://www.cleverbot.com/getreply" + "?key=" + settings.cleverbot_token + "&input=" + input;
-    //         if(clevState["cs"]) queryUrl += "&cs=" + clevState["cs"];
-    //         const request = http.get(queryUrl, (response) => {
-    //             let body = "";
-    //             response.on("data", (buf) => {
-    //                 body += buf;
-    //             });
-    //             response.on("end", (err) => {
-    //                 if(err) console.log(err);
-    //                 const clevObj;
-    //                 try {
-    //                     clevObj = JSON.parse(body.toString());
-    //                 }
-    //                 catch(error){
-    //                     clevState["cs"] = "";
-    //                     fs.writeFile("cleverbot.json", JSON.stringify(clevState), function(err) { if (err) console.log(err); });
-    //                     message.channel.send(message.author + ", " + "...");
-    //                     return;
-    //                 }
-    //                 clevState["cs"] = clevObj["cs"];
-    //                 message.channel.send(message.author + ", " + clevObj["output"]);
-    //                 fs.writeFile("cleverbot.json", JSON.stringify(clevState), function(err) { if (err) console.log(err); });
-    //             });
-    //         });
-    //     });
-    // }
-
+    //ignore message if it doesn't begin with prefix
     if(message.content.indexOf(settings.prefix) !== 0) return;
+
+    //get command and arguemnts
     const args = message.content.slice(settings.prefix.length).split(/ +/g);
     const command = args.shift().toLowerCase();
 
     //Commands for SpritasBot
+    switch(command){
+        case "help":
+            let reply = "Current commands: ping, uptime, site, vcam, tutorial, parallax, flash, spritas\nMore details to be added...";           
+            replyToMessage(message, reply);
+            break;
+        case "ping":
+            // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+            // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+            let reply = "Ping?";
+            const m = replyToMessage(message, reply);
+            m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+            break;
+        case "uptime":
+            let reply = getClientUptimeResponse(message.client);
+            replyToMessage(message, reply);
+            break;
+        case "site":
+            let reply = "<https://www.thespritas.net/>";
+            replyToMessage(message, reply)
+            break;
+        case "vcam":
+        case "preloader":
+        case "intro":
+            let reply = "<https://www.thespritas.net/t7199-the-spritas-intro-vcams-and-preloaders>";
+            replyToMessage(message, reply);
+            break;
+        case "tutorial":
+            let reply = "<https://www.thespritas.net/t184-how-to-sprite-animate-with-flash>";
+            replyToMessage(message, reply);
+            break;
+        case "parallax":
+            let reply = "```onClipEvent (load) { \n\t//distance ranges from 0 to 1 \n\tvar distance:Number = 0.2; \n\t//ignore the below stuff \n\tvar x0:Number = this._x; \n\tvar y0:Number = this._y; \n\tvar vcamx0:Number = _root.vcam._x; \n\tvar vcamy0:Number = _root.vcam._y; \n} \n\nonClipEvent (enterFrame) { \n\t_x = (_root.vcam._x - vcamx0) * distance + x0; \n\t_y = (_root.vcam._y - vcamy0) * distance + y0; \n}```Tutorial: https://i.imgur.com/ZzfKHuB.png";
+            replyToMessage(message, reply);
+            break;
+        case "flash":
+            let reply = 'Flash MX = ...what year is it?';
+            reply += '\nFlash 8 - has a pacemaker and always tells you to get off its lawn';
+            reply += '\nFlash CS3 - literally Jesus';
+            reply += '\nFlash CS4 - thinks its an orphan, but its parents sold it for drug money';
+            reply += '\nFlash CS5 - probably should have been aborted';
+            reply += '\nFlash CS5.5 - definitely should have been aborted';
+            reply += '\nFlash CS6 - can be a bit of an asshole, but alright; eats all your food';
+            reply += '\nFlash CC - eats all your food and more but gives you a dollar if you ask for one';
+            reply += '\nAdobe Animate - hipster that thinks it\'s cool because it changed its name';
 
-    //!help
-    if(command === "help") {
-        message.channel.send("Current commands: ping, uptime, site, vcam, tutorial, parallax, flash, spritas"
-        + "\nMore details to be added...");
-    }
-
-    //!ping
-    if(command === "ping") {
-        // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-        // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-        const m = await message.channel.send("Ping?");
-        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    }
-
-    //!uptime
-    if(command === "uptime") {
-        let reply = 'Uptime: ';
-        let time = message.client.uptime / 1000;
-        let seconds = time % 60 * 1;
-        seconds = Math.floor(seconds);
-        time /= 60;
-        let minutes = time % 60 * 1;
-        minutes = Math.floor(minutes);
-        time /= 60;
-        let hours = time % 24 * 1;
-        hours = Math.floor(hours);
-        if(hours === 1) reply += hours + ' hour '; 
-        else reply += hours + ' hours ';
-        if(minutes === 1) reply += minutes + ' minute '; 
-        else reply += minutes + ' minutes ';
-        if(seconds === 1) reply += seconds + ' second.';
-        else reply += seconds + ' seconds.';
-        message.channel.send(reply);
-    }
-
-    //!site
-    if(command === "site") message.channel.send("<https://www.thespritas.net/>");
-
-    //!vcam, preloader, intro
-    if(command === "vcam" || command === "preloader" || command === "intro") message.channel.send('<https://www.thespritas.net/t7199-the-spritas-intro-vcams-and-preloaders>');
-
-    //!tutorial
-    if(command === "tutorial") message.channel.send('<https://www.thespritas.net/t184-how-to-sprite-animate-with-flash>');
-
-    //!parallax
-    if(command === "parallax") 
-        message.channel.send('```onClipEvent (load) { \n\t//distance ranges from 0 to 1 \n\tvar distance:Number = 0.2; \n\t//ignore the below stuff \n\tvar x0:Number = this._x; \n\tvar y0:Number = this._y; \n\tvar vcamx0:Number = _root.vcam._x; \n\tvar vcamy0:Number = _root.vcam._y; \n} \n\nonClipEvent (enterFrame) { \n\t_x = (_root.vcam._x - vcamx0) * distance + x0; \n\t_y = (_root.vcam._y - vcamy0) * distance + y0; \n}```Tutorial: https://i.imgur.com/ZzfKHuB.png');
-
-    //!flash
-    if(command === "flash") {
-        let flashResponse = 'Flash MX = ...what year is it?';
-	    flashResponse += '\nFlash 8 - has a pacemaker and always tells you to get off its lawn';
-        flashResponse += '\nFlash CS3 - literally Jesus';
-        flashResponse += '\nFlash CS4 - thinks its an orphan, but its parents sold it for drug money';
-        flashResponse += '\nFlash CS5 - probably should have been aborted';
-        flashResponse += '\nFlash CS5.5 - definitely should have been aborted';
-        flashResponse += '\nFlash CS6 - can be a bit of an asshole, but alright; eats all your food';
-        flashResponse += '\nFlash CC - eats all your food and more but gives you a dollar if you ask for one';
-        flashResponse += '\nAdobe Animate - hipster that thinks it\'s cool because it changed its name';
-
-        message.channel.sendMessage(flashResponse);
-    }
-
-    //!spritas
-    if(command === "spritas") {
-        //if message is in dm and message author does not have role make them a Spritan
-        if(message.channel.type == "dm" || message.channel.type == "group") {
-            const spritas = client.guilds.get(settings.spritas_server);
-            if(!spritas.available) return;
-            const spritans = spritas.roles.find('name', 'Spritans').id;
-            const mods = spritas.roles.find('name', 'Spritas Moderators').id;
-            const admins = spritas.roles.find('name', 'Spritas Admins').id;
-            const getMember = spritas.fetchMember(message.author);
-            getMember.then(member => {
-                let memberRoles = member.roles;
-                if(!memberRoles.has(spritans) && !memberRoles.has(mods) && !memberRoles.has(admins)) {
-                    member.addRole(spritans);
-                    message.channel.send('Role change success. You are now a Spritan! Happy Posting!')
-                        //.then(message => console.log(`Sent message: ${message.content} to ${message.author.username}`))
-                        .catch(console.error);
-                } else if(memberRoles.has(mods)) {
-                    message.channel.send('Role change succes- Wait a minute. You\'re a mod! I can\'t change your role!')
-                        //.then(message => console.log(`Sent message: ${message.content} to ${message.author.username}`))
-                        .catch(console.error);
-                } else if(memberRoles.has(admins)) {
-                    message.channel.send('Role change succes- Wait a minute. You\'re an admin! Uh, sorry sir. I can\'t change your role. Plz don\'t ban me.')
-                        //.then(message => console.log(`Sent message: ${message.content} to ${message.author.username}`))
-                        .catch(console.error);
-                } else if(memberRoles.has(spritans)){
-                    message.channel.send('Role change succes- Wait a minute. You\'re already a Spritan! Get outta here ya silly!')
-                        //.then(message => console.log(`Sent message: ${message.content} to ${message.author.username}`))
-                        .catch(console.error);
-                }
-            });     
-        } 
-        else {
-            message.reply('If you\'re trying to change your role to Spritan, then you need to send me a DM saying "!SPRITAS" and then I can change your role.');
-        }
+            replyToMessage(message, reply);
+            break;
+        case "spritas":
+            //if message is in dm and message author does not have role make them a Spritan
+            if(message.channel.type == "dm" || message.channel.type == "group") {
+                const spritas = client.guilds.get(settings.spritas_server);
+                if(!spritas.available) return;
+                const spritans = spritas.roles.find('name', 'Spritans').id;
+                const mods = spritas.roles.find('name', 'Spritas Moderators').id;
+                const admins = spritas.roles.find('name', 'Spritas Admins').id;
+                const getMember = spritas.fetchMember(message.author);
+                getMember.then(member => {
+                    let memberRoles = member.roles;
+                    if(!memberRoles.has(spritans) && !memberRoles.has(mods) && !memberRoles.has(admins)) {
+                        member.addRole(spritans);
+                        replyToMessage(message, "Role change success. You are now a Spritan! Happy Posting!");
+                    } 
+                    else if(memberRoles.has(mods))
+                        replyToMessage(message, "Role change succes- Wait a minute. You\'re a mod! I can\'t change your role!");                 
+                    else if(memberRoles.has(admins)) 
+                        replyToMessage(message, "Role change succes- Wait a minute. You\'re an admin! Uh, sorry sir. I can\'t change your role. Plz don\'t ban me.");
+                    else if(memberRoles.has(spritans))
+                        replyToMessage(message, "Role change succes- Wait a minute. You\'re already a Spritan! Get outta here ya silly!");                  
+                });     
+            } 
+            else 
+                replyToMessage(message, "If you\'re trying to change your role to Spritan, then you need to send me a DM saying \"!SPRITAS\" and then I can change your role.");       
+            break;
+        default:
+            break;
     }
 });
 
@@ -185,5 +128,24 @@ client.on("guildMemberAdd", (member) => {
     })
     .catch(console.error);
 });
+
+function replyToMessage(message, reply){
+    await message.channel.send(reply)
+        .then(message => console.log(`Sent message: ${message.content} to ${message.author.username}`))
+        .catch(console.error);
+    return message;
+}
+
+function getClientUptimeResponse(client){
+    let uptime = client.uptime / 1000; //total uptime in seconds
+    let days = Math.floor(uptime / 86400);
+    uptime %= 86400;
+    let hours = Math.floor(uptime / 3600)
+    uptime %= 3600;
+    let minutes = Math.floor(uptime / 60);
+    let seconds = uptime % 60;
+
+    return `SpritasBot has been up for: ${days} day(s), ${hours} hour(s), ${minutes} minute(s), ${seconds} second(s).`;
+}
 
 client.login(settings.discord_token);
