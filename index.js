acb21c0cc27a4d9073886af951f558428d1d32a2
Rapidfir3Pho3nix <official.rapidfir3pho3nix@gmail.com>
2019-09-20 17:24:43 -0500

    better error handling while playing music

--- index.js
+++ index.js
@@ -5,87 +5,81 @@
 const fs = require("fs");
 const csv = require("csv-parser");
 const Discord = require("discord.js");
-const { Util } = require('discord.js');
 const { prefix, discord_token, spritas_server, completed_channel, music_channel, spritas_youtube_reminder} = require("./config.json");
-const ytdl = require("ytdl-core");
 const ytdlDiscord = require("ytdl-core-discord");
 const prism = require('prism-media');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+// song queue
 queue = [];
+
+// current song index
 queueIndex = 0;
+
+// continue playing music
 playMusic = true;
 
+// read commands from files in command directory
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
+// set up functionality when bot is ready
 client.on("ready", () => {
     console.log("Bot is ready!");
     const spritas = client.guilds.get(spritas_server);
     const reminderDelay = 2 * 60 * 60 * 1000;
 
+    // set interval for reminder message in #completed channel
     client.setInterval(function(completedChan, reminder) {
+
+        // read config for previous reminder message ID
         let config = JSON.parse(fs.readFileSync('./config.json'));
         let pastMessageID = config.reminder_message_id;
+
+        // if current latest message ID does not match the ID for the last reminder message, delete the last reminder message, resend a new reminder message, and store new reminder message ID in config file
         if (completedChan.lastMessageID != pastMessageID) {
-            completedChan.fetchMessage(pastMessageID)
-                .then(pastMessage => {
-                    if (pastMessage) pastMessage.delete();
-                    completedChan.send(reminder)
-                        .then(message => {
-                            config.reminder_message_id = message.id;
-                            fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
-                        })
-                        .catch(console.error);
+            completedChan.fetchMessage(pastMessageID).then(pastMessage => {
+                console.log("grabbed previous message:", pastMessageID);
+                pastMessage.delete().then(msg => {
+                    console.log("previous message deleted:", msg);
+                    completedChan.send(reminder).then(message => {
+                        console.log("sent new message:", message.id)
+                        config.reminder_message_id = message.id;
+                        fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
+                    })
+                    .catch(sendError => { console.log("Error encountered while sending new reminder message:", sendError); });
                 })
-                .catch(err => {
-                    if (err.message == '404: Not Found') {
-                        completedChan.send(reminder)
-                        .then(message => {
-                            config.reminder_message_id = message.id;
-                            fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
-                        })
-                        .catch(console.error);
-                    } 
-                    else {
-                        spritas.owner.createDM().then((channel) => { channel.send(error); });
-                    }
-                });    
+                .catch(deleteError => { console.log("Error encountered while deleting previous reminder message:", deleteError); });                  
+            })
+            .catch(fetchError => {
+                if (fetchError.message == '404: Not Found') {
+                    completedChan.send(reminder).then(message => {
+                        config.reminder_message_id = message.id;
+                        fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
+                    })
+                    .catch(sendError => { console.log("Error encountered while sending new reminder message after not finding previous message:", sendError); });
+                }
+                else { console.log("Error encountered while fetching previous reminder message:", fetchError); }
+            });    
         }
     }, reminderDelay, spritas.channels.get(completed_channel), spritas_youtube_reminder);
 
-    //read playlist url
-    fs.createReadStream('./playlist.csv')
-        .pipe(csv())
-        .on('data', (row) => {
-            queue.push(row["Video URL"]);
-        })
-        .on('end', () => {
-            shuffle(queue);
-        });
+    // read playlist into queue and shuffle the queue
+    fs.createReadStream('./playlist.csv').pipe(csv()).on('data', (row) => {
+        queue.push({ url: row["Video URL"], title: row["Title"] });
+    })
+    .on('end', () => { shuffle(queue); });
 
-    try {
-        //join music channel and keep playing music until stop command
-        let musicChan = spritas.channels.get(music_channel);
-        musicChan.join().then(connection => {
-            try {
-                playMusicStream(connection);
-            } 
-            catch(err){
-                console.log(err)
-            }
-        });
-    }
-    catch (error){
-        console.log(error);
-        spritas.owner.createDM().then((channel) => { channel.send(error); });
-    }
+    // join music channel and play music
+    spritas.channels.get(music_channel).join().then(connection => playMusicStream(connection))
+    .catch(joinError => {
+        console.log("Error encountered while trying to join music void channel:", joinError);
+    });
 });
 
 client.on("message", async (message) => {
@@ -95,35 +89,6 @@ client.on("message", async (message) => {
     //get command and arguemnts
     const args = message.content.slice(prefix.length).split(/ +/g);
     const commandName = args.shift().toLowerCase();
-
-    if (message.author.username.indexOf("Rapidfir3Pho3nix") >= 0 && commandName == "stop") {
-        playMusic = false;
-        return;
-    }
-
-    if (message.author.username.indexOf("Rapidfir3Pho3nix") >= 0 && commandName == "play") {
-        playMusic = true;
-        shuffle(queue);
-        const spritas = client.guilds.get(spritas_server);
-        try {
-            //join music channel and keep playing music until stop command
-            let musicChan = spritas.channels.get(music_channel);
-            musicChan.join().then(connection => {
-                try {
-                    console.log("playing music");
-                    playMusicStream(connection);
-                } 
-                catch(err){
-                    console.log(err)
-                }
-            });
-        }
-        catch (error){
-            console.log(error);
-            spritas.owner.createDM().then((channel) => { channel.send(error); });
-        }
-        return;
-    }
 
     //if command does not exist return
     if (!client.commands.has(commandName)) return;
@@ -167,48 +132,43 @@ client.on("guildMemberAdd", async (member) => {
 client.login(discord_token);
 
 function playMusicStream(voiceConnection) {
-    try {
-        let url = queue[queueIndex];
-        ytdl.getInfo(url).then(songInfo => {
-            const song = {
-                id: songInfo.video_id,
-                title: Util.escapeMarkdown(songInfo.title),
-                url: songInfo.video_url
-            };
-            client.user.setPresence({ game: { name: "playing " + song.title }});
+    let song = queue[queueIndex];
+    console.log("grabbed next song from queue:", song);
 
-            ytdlDiscord(song.url).then(input => {
-                try {
-                    const pcm = input.pipe(new prism.opus.Decoder({ rate: 48000, channels: 2, frameSize: 960 }));
-                    let dispatcher = voiceConnection.playConvertedStream(pcm);
-                    dispatcher.setVolumeLogarithmic(0.25);
+    client.user.setPresence({ game: { name: song.title }});
+    console.log("presence set to:", song.title);
 
-                    dispatcher.on("end", (reason) => {
-                        dispatcher = null;
-                        queueIndex = (queueIndex + 1) % queue.length;
-                        if (queueIndex == 0) shuffle(queue);
-                        if (playMusic) playMusicStream(voiceConnection);
-                        else {
-                            voiceConnection.channel.leave();
-                        }
-                    })
-                    .on('error', (error) => {
-                        dispatcher = null;
-                        console.error(error);
-                        voiceConnection.channel.leave();
-                        const spritas = voiceConnection.client.guilds.get(spritas_server);
-                        spritas.owner.createDM().then((channel) => { channel.send(error); });
-                    });
-                }
-                catch (error) {
-                    console.log(error);
-                }
-            });
+    ytdlDiscord(song.url).then(input => {
+        console.log("playing song");
+        const pcm = input.pipe(new prism.opus.Decoder({ rate: 48000, channels: 2, frameSize: 960 }));
+        let dispatcher = voiceConnection.playConvertedStream(pcm);
+        dispatcher.setVolumeLogarithmic(0.25);
+
+        dispatcher.on("end", (reason) => {
+            console.log("song ended")
+            dispatcher = null;
+            playNextSong(voiceConnection);
+        })
+        .on('error', (error) => {
+            console.log("dispatcher error, skipping song:", error);
+            dispatcher = null;
+            playNextSong(voiceConnection)
         });
-    }
-    catch(err) {
-        console.error(err);
-        spritas.owner.createDM().then((channel) => { channel.send(err); });
+    })
+    .catch(error => {
+        console.log("error encountered using input from ytdldiscord:", error);
+        console.log("songRes:", songRes);
+        console.log("input:", input);
+        playNextSong(voiceConnection)
+    });
+}
+
+function playNextSong(voiceConnection) {
+    queueIndex = (queueIndex + 1) % queue.length;
+    if (queueIndex == 0) shuffle(queue);
+    if (playMusic) playMusicStream(voiceConnection);
+    else {
+        voiceConnection.channel.leave();
     }
 }
 
